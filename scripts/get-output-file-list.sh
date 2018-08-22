c57ada78e56fe70ada2556055e941d106151e517
Daniel Ecer <de-code@users.noreply.github.com>
2018-08-22 16:35:42 +0100

    make source file column configurable for get output file list

--- scripts/get-output-file-list.sh
+++ scripts/get-output-file-list.sh
@@ -7,7 +7,7 @@ cd ../sciencebeam
 python -m sciencebeam_gym.preprocess.get_output_files \
   --source-base-path=$DATA_URL \
   --source-file-list=$DATA_URL/$FILE_LIST \
-  --source-file-column=source_url \
+  --source-file-column=$SOURCE_FILE_COLUMN \
   --output-file-list=$RESULTS_URL/$OUTPUT_FILE_LIST \
   --output-file-suffix=$OUTPUT_FILE_SUFFIX \
   --output-base-path=$RESULTS_URL \
